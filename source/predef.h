f9e7c309f7af546dfd852f1bb16ee4951f425237
Uzi Wang <429373960@qq.com>
2024-01-02 00:57:00 +0800

    Refactor code
    * Refactor predef.h

--- source/predef.h
+++ source/predef.h
@@ -1,22 +1,29 @@
 #pragma once
 
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #include <Windows.h>
+
 #include <concurrent_vector.h>
 #include <d3d12.h>
 #include <dxgi1_6.h>
-#include <winnt.h> // LARGE_INTEGER
 #include <wrl.h>
 
-#include <cstdint>
-#include <cstddef>
-#include <algorithm>
 #include <atomic>
-#include <functional>
+#include <cstddef>
+#include <cstdint>
 #include <memory>
+#include <utility>
 #include <vector>
 
 #include "imgui.h"
-#include "imgui_impl_win32.h"
 #include "imgui_impl_dx12.h"
+#include "imgui_impl_win32.h"
 
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
