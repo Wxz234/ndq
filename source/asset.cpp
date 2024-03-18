b1ed7b563900e917a837a4c8a9b16fc3e1aa35ff
Uzi Wang <429373960@qq.com>
2024-03-18 18:04:17 +0800

    Update asset.cpp
    * Create Asset

--- source/asset.cpp
+++ source/asset.cpp
@@ -6,8 +6,14 @@ export module ndq:asset;
 
 namespace ndq
 {
+    enum class ASSET_TYPE
+    {
+        STATIC_MODEL
+    };
+
     class Asset
     {
-
+    public:
+        virtual ASSET_TYPE GetType() const = 0;
     };
 }
