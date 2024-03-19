38ff18576159c170e930d4cc1fd69d2b4e9ab43e
Uzi Wang <429373960@qq.com>
2024-03-20 04:16:49 +0800

    Update asset.cpp

--- source/asset.cpp
+++ source/asset.cpp
@@ -8,12 +8,38 @@ namespace ndq
 {
     enum class ASSET_TYPE
     {
-        STATIC_MODEL
+        STATIC_MODEL,
     };
 
-    class Asset
+    class IAsset
     {
     public:
         virtual ASSET_TYPE GetType() const = 0;
     };
+
+    IAsset* LoadStaticModel(const char* path);
+
+    void RemoveAsset(IAsset* pAsset);
+}
+
+namespace ndq
+{
+    class Asset : public IAsset
+    {
+    public:
+        virtual ~Asset() {}
+    };
+}
+
+namespace ndq
+{
+    IAsset* LoadStaticModel(const char* path)
+    {
+        return nullptr;
+    }
+
+    void RemoveAsset(IAsset* pAsset)
+    {
+
+    }
 }
