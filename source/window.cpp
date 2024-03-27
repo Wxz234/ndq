62a32496d96fb3162ad4413daf711449a3b6cd1d
Uzi Wang <429373960@qq.com>
2024-03-27 14:46:27 +0800

    Update header

--- source/window.cpp
+++ source/window.cpp
@@ -1,9 +1,6 @@
 module;
 
 #include "predef.h"
-#include "imgui.h"
-#include "imgui_impl_dx12.h"
-#include "imgui_impl_win32.h"
 #include "window.h"
 
 export module ndq:window;
--- source/window.h
+++ source/window.h
@@ -1,2 +1,2 @@
 #pragma once
-LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
