120e73f1830919e12b3f55bd6b046949a5460989
Uzi Wang <429373960@qq.com>
2025-02-15 11:31:53 +0800

    Update uuid.cpp
    * fix bug

--- source/core/uuid.cpp
+++ source/core/uuid.cpp
@@ -2,24 +2,27 @@
 
 #include <rpc.h>
 
+#include <memory>
+
 namespace ndq
 {
+    void UUIDStrDeleter(RPC_CSTR* str)
+    {
+        RpcStringFreeA(str);
+        delete str;
+    }
+
     class UUID::DATA
     {
     public:
-        DATA()
+        DATA() : uuid{}, uuidStr(new RPC_CSTR, UUIDStrDeleter)
         {
             UuidCreate(&uuid);
-            UuidToStringA(&uuid, &uuidStr);
-        }
-
-        ~DATA()
-        {
-            RpcStringFreeA(&uuidStr);
+            UuidToStringA(&uuid, uuidStr.get());
         }
 
         ::UUID uuid;
-        RPC_CSTR uuidStr;
+        std::shared_ptr<RPC_CSTR> uuidStr;
     };
 
     UUID::UUID()
@@ -31,23 +34,27 @@ namespace ndq
     {
         pimpl = new UUID::DATA;
         pimpl->uuid = u.pimpl->uuid;
+        pimpl->uuidStr = u.pimpl->uuidStr;
     }
 
     UUID::UUID(UUID&& u) noexcept
     {
         pimpl = new UUID::DATA;
         pimpl->uuid = u.pimpl->uuid;
+        pimpl->uuidStr = u.pimpl->uuidStr;
     }
 
     UUID& UUID::operator=(const UUID& u)
     {
         pimpl->uuid = u.pimpl->uuid;
+        pimpl->uuidStr = u.pimpl->uuidStr;
         return *this;
     }
 
     UUID& UUID::operator= (UUID&& u) noexcept
     {
         pimpl->uuid = u.pimpl->uuid;
+        pimpl->uuidStr = u.pimpl->uuidStr;
         return *this;
     }
 
@@ -70,6 +77,6 @@ namespace ndq
 
     std::string UUID::ToString() const
     {
-        return std::string(reinterpret_cast<char*>(pimpl->uuidStr));
+        return std::string(reinterpret_cast<char*>(*pimpl->uuidStr));
     }
 }
